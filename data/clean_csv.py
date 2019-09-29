7c881d7ee83bfdb4b53311c5e8f9cac59c96c27d
k47ha <kgross@uos.de>
2019-09-29 20:09:17 +0200

    starting idea to clean

---
+++ data/clean_csv.py
@@ -0,0 +1,15 @@
+import csv
+import json
+
+with open('CDD.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter='\t')
+    for row in reader:
+        header = row.keys()
+        print(row["PLACE"])
+        break
+    with open("CDD_cleaned.csv", mode="w") as outfile:
+        csv_columns = header
+        writer = csv.DictWriter(outfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for data in reader:
+            writer.writerow(data)
--- data/convert2json.py
+++ data/convert2json.py
@@ -1,2 +1,24 @@
 import csv
 import json
+from geojson import Feature, FeatureCollection, Point
+
+features = []
+
+with open('CDD_cleaned.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ')
+
+    for latitude, longitude, weather, temp in reader:
+        latitude, longitude = map(float, (latitude, longitude))
+        features.append(
+            Feature(
+                geometry=Point((longitude, latitude)),
+                properties={
+                    'weather': weather,
+                    'temp': temp
+                }
+            )
+        )
+
+collection = FeatureCollection(features)
+with open("GeoObsCDD.json", "w") as f:
+    f.write('%s' % collection)

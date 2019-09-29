2fa2f6939e07e546507d167b28709fd3e9b68093
k47ha <kgross@uos.de>
2019-09-29 21:43:48 +0200

    starting

---
+++ data/convert2pandas.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import csv
+
+
+def process_coordinate_string(str):
+    """
+    Take the coordinate string from the KML file, and break it up into [Lat,Lon,Lat,Lon...] for a CSV row
+    """
+    space_splits = str.split(" ")
+    ret = []
+    # There was a space in between <coordinates>" "-80.123...... hence the [1:]
+    for split in space_splits[1:]:
+        comma_split = split.split(',')
+        ret.append(comma_split[1])    # lat
+        ret.append(comma_split[0])    # lng
+    return ret
+
+
+with open('EventMapLayer', 'r') as f:
+    data = BeautifulSoup(f, 'lxml')
+
+    print(data)
+
+    """
+    with open('out.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        for coords in s.find_all('coordinates'):
+            writer.writerow(process_coordinate_string(coords.string))
+    """

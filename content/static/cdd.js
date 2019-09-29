58fc257c66954cee9b839b58a6436ce35cc666bd
k47ha <kgross@uos.de>
2019-09-29 14:31:23 +0200

    first working map

---
+++ content/static/cdd.js
@@ -0,0 +1,12 @@
+function initMap() {
+    // set view
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+    // initialize map
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiazQ3aGEiLCJhIjoiY2sxNHk2cmU2MG42dDNkcHlxdG5vMndwaSJ9.JCFRrLYlYnAtUXwpaqXKtw'
+    }).addTo(mymap);
+}

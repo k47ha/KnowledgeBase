959aefc528091d40ab78b4aaf11c7d319c1fd124
k47ha <kgross@uos.de>
2019-09-29 19:04:54 +0200

    adding popup

--- content/static/cdd.js
+++ content/static/cdd.js
@@ -9,4 +9,15 @@ function initMap() {
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoiazQ3aGEiLCJhIjoiY2sxNHk2cmU2MG42dDNkcHlxdG5vMndwaSJ9.JCFRrLYlYnAtUXwpaqXKtw'
     }).addTo(mymap);
+
+    // test marker
+    var marker = L.marker([49.2827, -123.1207]).addTo(mymap);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
+
+    function onMapClick(e) {
+        document.getElementById("click").innerHTML = e.latlng;
+    }
+
+    mymap.on('click', onMapClick);
 }
+
